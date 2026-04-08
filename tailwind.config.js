ad78c9141e65b87abd4ea2c3218e635e3ed01381
defcarlosasnp <87336607+defcarlosasnp@users.noreply.github.com>
2026-04-08 15:02:49 -0400

    Fix: estilos globales y dependencias de postcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,6 @@
 module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "./js/**/*.{js,ts,jsx,tsx,mdx}", // Agregamos la carpeta js por si acaso
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./index.html",
   ],
   theme: {
     extend: {},
