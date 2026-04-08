59f76067a64730015e5f5473a4da1f71097b5ac4
defcarlosasnp <87336607+defcarlosasnp@users.noreply.github.com>
2026-04-08 15:13:36 -0400

    Fix: Tailwind config broad scan and postcss return

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,9 @@
 module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./*.{js,ts,jsx,tsx,mdx}", // Por si hay archivos en la raíz
   ],
   theme: {
     extend: {},
