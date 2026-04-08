12ba3aeedade827c9e0c7fb4a9a3c7a22b0b90d6
defcarlosasnp <87336607+defcarlosasnp@users.noreply.github.com>
2026-04-08 15:16:38 -0400

    Update: Next.js a version segura y fix Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,7 @@ module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./*.{js,ts,jsx,tsx,mdx}", // Por si hay archivos en la raíz
+    "./components/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {},
